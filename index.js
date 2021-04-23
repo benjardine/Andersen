87efd3a8ce5eb8b8191c80d5d8557ec068b58efb
Ben Jardine <jardine_199@hotmail.co.uk>
2021-04-23 11:08:03 +0100

    sample react form created- no date or location added yet

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// component file
+import DayPicker from "DatePicker"
+
+//stylesheet
+import "style.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DayPicker />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
